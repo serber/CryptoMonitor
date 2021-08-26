7fb68a9eeac5f1d300119a50d6864abf01b3eb00
Albert Zakiev <albert.zakiev@gmail.com>
2021-08-26 23:32:02 +0300

    Mongo init

--- db/mongo/init_database.js
+++ db/mongo/init_database.js
@@ -1,7 +1,14 @@
 let res = [
-  db.createCollection('user');
-  db.createCollection('symbol_price');
-  db.createCollection('drop_price');
+  db.createCollection('user'),
+  db.createCollection('symbol_price'),
+  db.createCollection('drop_price'),
+  
+  db.user.insert(
+  {
+    "_id": "c11bb741-746f-4b98-8c33-264047d55411",
+    "login": "admin",
+    "password_hash": "l2Y5necZdbFHbwBHjLsXgKsL6bgyzYReWdUv5QzEjyg="
+  })
 ];
 
 printjson(res);
